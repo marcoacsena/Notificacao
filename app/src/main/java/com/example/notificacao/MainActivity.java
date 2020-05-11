9ebd5a8cb4f8c11d9ee72c7f081b41e7cd6658ca
MarcoSena <marcoacsena@gmail.com>
2020-05-11 17:56:36 -0300

    Acesso a site externo.

--- app/src/main/java/com/example/notificacao/MainActivity.java
+++ app/src/main/java/com/example/notificacao/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.notificacao;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvPesquisarObjeto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseMessaging.getInstance().subscribeToTopic("brasileira");
+
+        final String url = "www.correios.com.br";
+
+        tvPesquisarObjeto = findViewById(R.id.tvPesquisarObjeto);
+        tvPesquisarObjeto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String url = "http://correios.com.br/";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
